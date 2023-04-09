a8f11a4312b873a2be9dbf714e14160d1f312c01
Akzhantai <musabekovaanara17@gmail.com>
2023-04-09 12:28:38 +0600

    Problem 10

--- src/Problem10.java
+++ src/Problem10.java
@@ -1,4 +1,6 @@
 public class Problem10 {
+    // a and b - input numbers
+    // return - returns great common divisor of a and b
     public static int calculateGCD(int a, int b) {
         if (a == 0)
             return b;
