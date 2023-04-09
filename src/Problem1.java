117065256952437ff8cd897fefdcef5f5498888c
Akzhantai <musabekovaanara17@gmail.com>
2023-04-09 10:05:33 +0600

    Problem 1

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,5 +1,8 @@
 public class Problem1 {
-
+    // n - length of array
+    // arr - array
+    // min - minimum number in the array
+    // return - returns minimum number in the array
     public static int findMin(int[] arr, int n) {
         if (n == 1) {
             return arr[0];
