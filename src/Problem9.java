0e7bd1fb733f6ad3a737d45c6837a7777338d8d8
Akzhantai <musabekovaanara17@gmail.com>
2023-04-09 12:25:05 +0600

    Problem 9

--- src/Problem9.java
+++ src/Problem9.java
@@ -1,9 +1,13 @@
 public class Problem9 {
-        public static int calculateBinomialCoefficient(int n, int k) {
-            if (k == 0 || k == n) {
-                return 1;
-            } else {
-                return calculateBinomialCoefficient(n - 1, k - 1) + calculateBinomialCoefficient(n - 1, k);
-            }
+    // basis: (n!)/(((n-k)!)*(k!))
+    // n - input number
+    // k - input number
+    // return - returns binomial coefficient of n and k
+    public static int calculateBinomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        } else {
+            return calculateBinomialCoefficient(n - 1, k - 1) + calculateBinomialCoefficient(n - 1, k);
         }
     }
+}
