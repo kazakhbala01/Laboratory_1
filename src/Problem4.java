65f151486a56845c1524b1c4d96962d4b43ab66d
Akzhantai <musabekovaanara17@gmail.com>
2023-04-09 10:27:51 +0600

    Problem 4

---
+++ src/Problem4.java
@@ -0,0 +1,11 @@
+class Problem4 {
+    // n - input number
+    // return - returns the factorial of n
+    public static int calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
