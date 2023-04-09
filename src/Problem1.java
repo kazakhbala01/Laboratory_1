27a2fa053570763bb7daab2fa9d6a678774ada43
Akzhantai <musabekovaanara17@gmail.com>
2023-04-09 10:02:17 +0600

    Problem 1

---
+++ src/Problem1.java
@@ -0,0 +1,11 @@
+public class Problem1 {
+
+    public static int findMin(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        } else {
+            int min = findMin(arr, n - 1);
+            return Math.min(min, arr[n - 1]);
+        }
+    }
+}
